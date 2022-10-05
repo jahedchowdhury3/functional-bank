bbed26b4b953df51a43cf892c0b1f01350119b15
jahedchowdhury3 <jahedchowdhury3@gmail.com>
2022-10-05 11:49:35 +0600

    deposit added

--- js/banking.js
+++ js/banking.js
@@ -1,1 +1,12 @@
-console.log('javasrcipt added');
+document.getElementById('deposit-button').addEventListener('click', function () {
+    const depositInput = document.getElementById('deposit-input');
+    const depositAmount = depositInput.value;
+
+    // get current deposit total
+    const depositTotal = document.getElementById('deposit-total');
+    const depositTotalText = depositTotal.innerText;
+    depositTotal.innerText = depositAmount;
+    console.log(depositTotalText);
+    // clear the field
+    depositInput.value = '';
+})
