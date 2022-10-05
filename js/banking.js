89cdbe4d763a27915d0706bd9899879789da5ed2
jahedchowdhury3 <jahedchowdhury3@gmail.com>
2022-10-05 12:11:24 +0600

    deposit clear

--- js/banking.js
+++ js/banking.js
@@ -1,12 +1,23 @@
 document.getElementById('deposit-button').addEventListener('click', function () {
     const depositInput = document.getElementById('deposit-input');
-    const depositAmount = depositInput.value;
+    const depositAmountText = depositInput.value;
+    const depositAmount = parseFloat(depositAmountText);
 
     // get current deposit total
     const depositTotal = document.getElementById('deposit-total');
+
     const depositTotalText = depositTotal.innerText;
-    depositTotal.innerText = depositAmount;
-    console.log(depositTotalText);
+
+    const previousDepositTotal = parseFloat(depositTotalText);
+
+    depositTotal.innerText = depositAmount + previousDepositTotal;
+
+    // update balance
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    balanceTotal.innerText = depositAmount + previousBalanceTotal;
+
     // clear the field
     depositInput.value = '';
 })
