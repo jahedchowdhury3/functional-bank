7075218292ebec5810aa93da8edd4b44b28f730e
jahedchowdhury3 <jahedchowdhury3@gmail.com>
2022-10-18 20:25:49 +0600

    second commit

--- js/functional.js
+++ js/functional.js
@@ -1,1 +1,51 @@
-console.log('333');
+function getInputValue(feildId) {
+    const inputField = document.getElementById(feildId);
+    const valueInText = inputField.value;
+    const value = parseFloat(valueInText);
+    inputField.value = '';
+    return value;
+}
+function getInnerTextValue(feildId) {
+    const fieldTag = document.getElementById(feildId);
+    const fieldValueInText = fieldTag.innerText;
+    const value = parseFloat(fieldValueInText);
+    return value;
+}
+function updateTotal(feildId, amount) {
+    const totalTag = document.getElementById(feildId);
+    const previousTotalInText = totalTag.innerText;
+    const previousTotal = parseFloat(previousTotalInText);
+    const newTotal = previousTotal + amount;
+    totalTag.innerText = newTotal;
+}
+// handle balance
+function updateBalance(amount, isAdding) {
+    const balanceTag = document.getElementById('balance-total');
+    const balanceInText = balanceTag.innerText;
+    const previousBalance = parseFloat(balanceInText);
+    let newBalance;
+    if (isAdding == true) {
+        newBalance = previousBalance + amount;
+    }
+    else {
+        newBalance = previousBalance - amount;
+    }
+    balanceTag.innerText = newBalance;
+}
+// handle deposit
+document.getElementById('deposit-button').addEventListener('click', function () {
+    const amount = getInputValue('deposit-input');
+    if (amount > 0) {
+        updateTotal('deposit-total', amount);
+        updateBalance(amount, true);
+    }
+})
+// handle withdraw
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    const amount = getInputValue('withdraw-input');
+    const balance = getInnerTextValue('balance-total');
+    if (amount > 0 && amount <= balance) {
+        updateTotal('withdraw-total', amount);
+        updateBalance(amount, false);
+    }
+})
